edee5fadbe5b1c5dd91586701b766c70d48667bb
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-26 00:34:20 -0700

    Initial Commit

---
+++ EspecializacionSwiftUI/EspecializacionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EspecializacionSwiftUIApp.swift
+//  EspecializacionSwiftUI
+//
+//  Created by Juan Villalvazo on 26/05/21.
+//
+
+import SwiftUI
+
+@main
+struct EspecializacionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
