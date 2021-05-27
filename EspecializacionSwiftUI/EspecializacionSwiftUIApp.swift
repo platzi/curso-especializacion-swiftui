7e06c8e832d12660a0e3e2321751d80d297e44d8
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-27 01:11:57 -0700

    Branch mainInitApp

--- EspecializacionSwiftUI/EspecializacionSwiftUIApp.swift
+++ EspecializacionSwiftUI/EspecializacionSwiftUIApp.swift
@@ -7,11 +7,29 @@
 
 import SwiftUI
 
+//Al igual que los property wrapper que utilizamos en clases anteriores este @Main le da "cierta habilidad" a nuestro elemento, en este caso al struct, el cual gracias a @Mail su protocolo App puede implementar la funcion main de la app
 @main
 struct EspecializacionSwiftUIApp: App {
+    
+    
+    //En init Puedes ejecutar codigo antes de que cualquier UI se muestre al usuario
+    init() {
+        
+    }
+    
+    //Body contendra una Scena que en este caso es la pantalla ContentView la cual esta embebida en un windowsGroup
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
+            // Dentro de un windowGroup puedes tener mas de una instancia de manera independiente y paralela (ejemplo en ipad)
+            HStack {
+                ContentView()
+                ContentView()
+            }
+            
         }
     }
+    
+    
+    
 }
